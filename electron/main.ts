ad950d90dd71632b67608c11441e41c2b3fdc5af
Ronny Fretel <ronny@fretelweb.com>
2020-03-28 12:55:39 -0500

    add Integrando angular en electron

---
+++ electron/main.ts
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from 'electron'
+import * as path from 'path'
+import * as url from 'url'
+
+let win: BrowserWindow
+
+app.on('ready', createWindow)
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
+
+  win.maximize()
+
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `/../../dist/angular-app/index.html`),
+      protocol: 'file:',
+      slashes: true,
+    })
+  )
+
+  win.webContents.openDevTools()
+
+  win.on('closed', () => {
+    win = null
+  })
+}
