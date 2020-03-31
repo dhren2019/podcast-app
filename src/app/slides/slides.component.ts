32e0fd6ffc3aa37a63f421c5254c6fb212794466
Ronny Fretel <ronny@fretelweb.com>
2020-03-31 11:22:23 -0500

    Slider con cards de BS4

--- src/app/slides/slides.component.ts
+++ src/app/slides/slides.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlidesComponent implements OnInit {
 
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
   constructor() { }
 
   ngOnInit() {
